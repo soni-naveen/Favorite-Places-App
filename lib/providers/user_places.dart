6552686b4ab59ece2bedb4da8f6caad250f3941e
Naveen Soni <naveennsonii@gmail.com>
2026-01-21 23:39:36 +0530

    feat: implemented modern riverpod provider

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,8 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favorite_places_app/models/place.dart';
-import 'package:flutter_riverpod/legacy.dart';
 
-class UserPlacesNotifier extends StateNotifier<List<Place>> {
-  UserPlacesNotifier() : super(const []);
+class UserPlacesNotifier extends Notifier<List<Place>> {
+  @override
+  List<Place> build() {
+    return [];
+  }
 
   void addPlace(String title) {
     final newPlace = Place(title: title);
@@ -10,7 +13,22 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   }
 }
 
-final userPlacesProvider =
-    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
-      (ref) => UserPlacesNotifier(),
-    );
+final userPlacesProvider = NotifierProvider<UserPlacesNotifier, List<Place>>(
+  UserPlacesNotifier.new,
+);
+
+// import 'package:flutter_riverpod/legacy.dart';
+
+// class UserPlacesNotifier extends StateNotifier<List<Place>> {
+//   UserPlacesNotifier() : super(const []);
+
+//   void addPlace(String title) {
+//     final newPlace = Place(title: title);
+//     state = [newPlace, ...state];
+//   }
+// }
+
+// final userPlacesProvider =
+//     StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+//       (ref) => UserPlacesNotifier(),
+//     );
